df4915ac64d9b4826c9cb3418184de54df05dcd3
Rocio Ramirez <ramirezrocio.bo@gmail.com>
2023-02-11 10:29:42 -0400

    Added search movies functionality

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ const movie1 = {
 const App = () => {
 
     const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`)
@@ -35,13 +36,13 @@ const App = () => {
             <div className="search">
                 <input 
                   placeholder="Search for movies"
-                  value="Superman"
-                  onChange={() => {}}
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
                 />
                 <img
                   src={SearchIcon}
                   alt="search"
-                  onClick={() => {}}
+                  onClick={() => searchMovies(searchTerm)}
                 />
 
             </div>
